5a90e14f911504a78bcd394bd419e3aafda70ded
Dharminder Dharna <dharminder@stellar.aero>
2017-10-31 15:28:20 -0700

    test db migration

--- organization-service/src/main/resources/db/migration/V001__init_schema.sql
+++ organization-service/src/main/resources/db/migration/V001__init_schema.sql
@@ -10,6 +10,33 @@ CREATE TABLE organization
      created_date       BYTEA, 
      last_modified_date BYTEA, 
      name               VARCHAR(255) NOT NULL,
-     address			VARCHAR(255),
      PRIMARY KEY (id) 
-  ) 
+  );
+  
+  INSERT INTO organization 
+            (id, 
+             NAME, 
+             contact_name, 
+             contact_email, 
+             contact_phone, 
+             active) 
+VALUES      ('e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 
+             'customer-crm-co', 
+             'Dharminder Dharna', 
+             'ddharna@custcrmco.com', 
+             '823-555-1212', 
+             true); 
+
+INSERT INTO organization 
+            (id, 
+             NAME, 
+             contact_name, 
+             contact_email, 
+             contact_phone, 
+             active) 
+VALUES      ('442adb6e-fa58-47f3-9ca2-ed1fecdfe86c', 
+             'HR-PowerSuite', 
+             'Doug Drewry', 
+             'doug.drewry@hr.com', 
+             '920-555-1212', 
+             true); 
